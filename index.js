acb036adf116d1b3198b3c5ec4579bc7094da2d4
Emanuelevit <emanuele.romualdo@escola.pr.gov.br>
2026-06-08 12:24:43 -0300

    funcao.array

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ let frutas = ["laranja","manga","maçã","maracuja","uva"];
  console.log(frutas[4]);
  
  let numeros = [10, 5, 8, 2, 7, 3]; 
- let soma = 0; let numeros = [1, 2, 3, 4, 5, 6]; 
- for (let i = 0; i < numeros.length; i++) { soma = soma + numeros[i]; }
+ let soma = 0; 
+ let numeros = [1, 2, 3, 4, 5, 6]; 
+ for (let i = 0; i < numeros.length; i++) { 
+    soma = soma + numeros[i]; 
+}
